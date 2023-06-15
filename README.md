f8f2bfb4137bff46f13a15a5514db8637b46b25a
FaizanKhanDev <faizan480khan@gmail.com>
2023-06-15 12:17:08 +0500

    i have add some Filteration function

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>JavaScript Lamba Function </h1>
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const obj = {
     'Student': [
         {
             "name": "Raj",
-            "Age": "15",
+            "Age": "12",
             "RollNumber": "123",
             "Marks": "99",
         },
@@ -18,12 +18,18 @@ const obj = {
             "RollNumber": "253",
             "Marks": "89",
         },
+        {
+            "name": "Vivek",
+            "Age": "16",
+            "RollNumber": "253",
+            "Marks": "89",
+        },
     ]
 };
 
 
 const newArray = obj.Student.filter(function (el) {
-    return el.Age >= 15 &&
+    return el.Age <= 15 &&
         el.RollNumber <= 200 &&
         el.Marks >= 80
 });
@@ -32,11 +38,35 @@ const againNewArray = obj.Student.filter(function (el) {
         el.Marks >= 99
 
 });
-console.log("=================", newArray, "=================")
-console.log("=================", againNewArray, "=================")
+console.log("=====newArray============", newArray, "===========newArray======")
+console.log("=====againNewArray============", againNewArray, "========againNewArray=========")
 
 
 const words = ["faizan", "khan", "developers", "vueJs/Nuxt", "Compiler"]
+
 const result = words.filter(word => word.length <= 6)
-console.log("====================", words, "=================")
-console.log("====================", result, "=================")
+const secondResult = words.filter(function (el) {
+    return el.length >= 5
+
+});
+console.log("=========== words =========", words, "========words =========")
+console.log("=========== result =========", result, "======result ===========")
+console.log("======== secondResult ============", secondResult, "========secondResult =========")
+
+
+const array1 = [5, 12, 8, 130, 44];
+
+
+const foundNewArray = array1.filter(ele => ele = 10);
+console.log("======== foundNewArray ============", foundNewArray, "======== foundNewArray ============")
+
+const faizan = ["faizan", "naveed", "khan"]
+console.log("========faizan======================", faizan, "=======faizan=======================");
+const newFaizan = faizan.filter(fa => fa.length === 6)
+
+console.log(newFaizan)
+
+const secondNewArray = faizan.filter(function (f) {
+    return f.length === 6
+})
+console.log("========secondNewArray======================", secondNewArray, "========secondNewArray======================",)
