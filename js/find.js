b3e689e24ff9e74bccaf7537b114af33623a9460
FaizanKhanDev <faizan480khan@gmail.com>
2023-06-15 15:23:12 +0500

    use Vue/nuxt/vuex object is find methods

--- js/find.js
+++ js/find.js
@@ -45,11 +45,37 @@ const isBlack = (color) => {
 console.log(markers.find(isBlack))
 
 
+const addressInfo = {
+    bussinessWebsiteName: "https://faizankhandev.netlify.app/",
+    regions: [
+        { name: "United", code: "US" },
+        { name: "Pakistan", code: "Pk" },
+        { name: "Canada", code: "CA" },
+    ],
+    provinces: [
+        { name: "Sindhi", code: "SI" },
+        { name: "Punjab", code: "PU" },
+        { name: "Khyber", code: "KY" },
+        { name: "balochistan", code: "BA" }
+    ],
+    cities: [
+        { name: "karachi", code: "KHI" },
+        { name: "Hyderabad", code: "HD" },
+    ],
+    completeAddress: "landhi no1 Area 37D house no 480 street no9 karachi",
+    companyPhoneNumber: "03162177746",
+    companyEmail: "faizan480khan@gmail.com",
+    postalCode: 123123,
+    selectedRegion: "Pk",
+    selectedCity: "KHI",
+    selectedProvince: "SI",
 
-// // ES5
-var x = function (x, y) {
-    return x * y;
 }
+const findAddressInfo = (elem) => {
+    return elem.name === "United"
+}
+const myFoundResults = addressInfo.regions.find(findAddressInfo)
+console.log(myFoundResults)
 
-// // ES6
-const x = (x, y) => x * y;
+const myResults = addressInfo.regions.length
+console.log(myResults)
---
+++ js/findIndex.js
@@ -0,0 +1,4 @@
+const findIndexOfArray = [12, 15, 123, 124]
+const newFindIndexOfArray = (elemt) => elemt > 100
+
+console.log(findIndexOfArray.findIndex(newFindIndexOfArray), "========================== Finding index =============")
